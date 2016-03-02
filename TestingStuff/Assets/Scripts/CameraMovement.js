05b2027ec2eafacf3a0c167d6fbb2d7a18438b82
Felix Hortelano <felix.hortelano@est.fib.upc.edu>
2016-03-02 16:11:30 +0100

    Initial commit

---
+++ TestingStuff/Assets/Scripts/CameraMovement.js
@@ -0,0 +1,15 @@
+﻿#pragma strict
+
+private var player : Transform;
+private var Yorg : Vector3;
+
+function Start () {
+	player= GameObject.FindGameObjectWithTag("Player").transform;
+	
+}
+
+function Update () {
+	var aux : Vector3 = new Vector3(-2.5, 5.0, 10.0);
+	transform.position = player.position + aux;
+	transform.LookAt(player);
+}
